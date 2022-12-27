ee39cf2e18856473cbfb9c699e79bc8f89298f77
pratyush Singha <pratyushsingha83@gmail.com>
2022-12-27 21:50:44 +0530

    Update and rename substring to substring.java

--- hacker_rank/substring.java
+++ hacker_rank/substring.java
@@ -7,7 +7,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class substring {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
